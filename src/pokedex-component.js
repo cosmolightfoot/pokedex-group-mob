abb77f4a8231b3cadfea67be4da0ed851cb795b1
Cosmo Kent <cosmolightfoot@gmail.com>
2019-02-28 13:28:41 -0800

    color test complete

--- src/pokedex-component.js
+++ src/pokedex-component.js
@@ -1,13 +1,13 @@
 export default function makeCard(pokemon){
     const pokeCase = pokemon.pokemon.charAt(0).toUpperCase() + pokemon.pokemon.slice(1);
     const html = /*html*/`
-    <li>
+    <li class="${pokemon.type_1}">
     <h3>${pokeCase}</h3>
     <img src="${pokemon.url_image}">
     <p>HP: ${pokemon.hp} A: ${pokemon.attack} D: ${pokemon.defense}</p></li>
 `;
 
-    
+
     const template = document.createElement('template');
     template.innerHTML = html;
     return template.content;
--- test/file1.test.js
+++ test/file1.test.js
@@ -2,22 +2,23 @@ import makeCard from '../src/pokedex-component.js';
 const test = QUnit.test;
 QUnit.module('image-component test');
 
-
+const pokemon = {
+    'pokemon': 'bulbasaur',
+    'attack': 49,
+    'defense': 49,
+    'hp': 45,
+    'url_image': 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+    'pokedex': 'http://www.pokemon.com/us/pokedex/bulbasaur',
+    'type_1': 'grass'
+};
 
 test('vuild image template', function(assert) {
     //arrange
-    const pokemon = {
-        'pokemon': 'bulbasaur',
-        'attack': 49,
-        'defense': 49,
-        'hp': 45,
-        'url_image': 'http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
-        'pokedex': 'http://www.pokemon.com/us/pokedex/bulbasaur'
-    };
+   
     //act
     const template = makeCard(pokemon);
     const expected = /*html*/`
-            <li>
+            <li class="grass">
                 <h3>Bulbasaur</h3>
                 <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png">
                 <p>HP: 45 A: 49 D: 49</p></li>
@@ -25,4 +26,22 @@ test('vuild image template', function(assert) {
     //assert
     
     assert.htmlEqual(template, expected);
+});
+
+test('add class that is same as type', assert => {
+    //arrange 
+
+    //act
+    const template = makeCard(pokemon);
+
+    const expected = /*html*/`
+    <li class="grass">
+        <h3>Bulbasaur</h3>
+        <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png">
+        <p>HP: 45 A: 49 D: 49</p></li>
+`;
+
+
+    //assert
+    assert.htmlEqual(template, expected)
 });
