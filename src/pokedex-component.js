5eb439bf90936cae66b727fcd9ac4fe237b2bb02
Cosmo Kent <cosmolightfoot@gmail.com>
2019-02-28 13:37:47 -0800

    colored by type

--- src/pokedex-component.js
+++ src/pokedex-component.js
@@ -1,7 +1,8 @@
 export default function makeCard(pokemon){
+    
     const pokeCase = pokemon.pokemon.charAt(0).toUpperCase() + pokemon.pokemon.slice(1);
     const html = /*html*/`
-    <li class="${pokemon.type_1}">
+    <li class="${pokemon.type_1}" style="background-color:${pokemon.color_1}">
     <h3>${pokeCase}</h3>
     <img src="${pokemon.url_image}">
     <p>HP: ${pokemon.hp} A: ${pokemon.attack} D: ${pokemon.defense}</p></li>
